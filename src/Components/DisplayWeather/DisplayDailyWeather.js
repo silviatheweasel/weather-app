153dacad3b05517608fabb1b7cddfae0b9467489
Xue Li <xueli@Xues-MBP.lan>
2021-05-06 22:36:56 +0100

    update api url

--- src/Components/DisplayWeather/DisplayDailyWeather.js
+++ src/Components/DisplayWeather/DisplayDailyWeather.js
@@ -11,7 +11,7 @@ export const DisplayDailyWeather = ({ dailyWeather }) => {
                         const { min, max } = temp;
                         const { icon } = weather[0];
                         const myDay = new Date(dt*1000).getDate() === new Date().getDate() ? "Today" : daysOfTheWeek[new Date(dt*1000).getDay()];
-                        const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+                        const iconUrl = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
 
                         return (<li key={dt}>
                             <span className="myDay">{myDay}</span>
